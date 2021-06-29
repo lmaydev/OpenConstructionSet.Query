21c721870aebfc5ca3e1394b67b9a2659f5a7efd
Luke <allinighdrui@gmail.com>
2021-06-29 12:48:27 +0100

    Project created

---
+++ OpenConstructionSet.Query/OpenConstructionSet.Query/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenConstructionSet.Query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
